9a3db95cf0131217a4d339980336ef16cb46a3bb
Phillip Malboeuf <phil@phils.computer>
2023-07-24 15:09:21 -0400

    sass variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "0.variables.scss";`
+  },
+}
 
 module.exports = nextConfig
